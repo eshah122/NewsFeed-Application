ccde466e0073bc3a4b56a062a669d0522ac44ba0
Esha H <54173101+eshah122@users.noreply.github.com>
2023-03-27 11:08:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
+let newsFeed = ['N1','N2','N3','N4','N5','N6','N7','N8','N9','N10'];
+
+let newsList = document.getElementById("news-list")
+for(let i =0;i<10;i++){
+    let listItem = document.createElement("li");
+    listItem.textContent = newsFeed[i];
+    newsList.appendChild(listItem);
+} 
+
+function begin(){
+    let newsList = document.getElementById("news-list")
+    newsList.innerHTML =" ";
+    for(let i =0;i<10;i++){
+        let listItem = document.createElement("li");
+        listItem.textContent = newsFeed[i];
+        newsList.appendChild(listItem);
+}
+}
+
+let submitBtn=document.getElementById("submit-btn");
+submitBtn.addEventListener("click",function(event){
+    event.preventDefault();
+    let newsInput = document.getElementById("news-input");
+    let newsHeadline = newsInput.value.trim();
+    newsFeed.unshift(newsHeadline);
+    newsInput.value='';
+    begin();
+    
+});
 
