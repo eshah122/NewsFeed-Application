0dab206ba8c29d8994c2cb0ab55c17f23370c291
Esha H <54173101+eshah122@users.noreply.github.com>
2023-03-27 11:43:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
 let newsFeed = ['N1','N2','N3','N4','N5','N6','N7','N8','N9','N10'];
 
-let newsList = document.getElementById("news-list")
-for(let i =0;i<10;i++){
-    let listItem = document.createElement("li");
-    listItem.textContent = newsFeed[i];
-    newsList.appendChild(listItem);
-} 
-
 function begin(){
     let newsList = document.getElementById("news-list")
     newsList.innerHTML =" ";
